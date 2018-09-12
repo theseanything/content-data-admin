727b0d99ac36622613be55a853ab223a4f1bdfee
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-09-12 11:31:05 +0100

    Refactor date_selection_spec
    Refactor date_selection_spec to improve readability and DRY up some tests.

--- spec/features/date_selection_spec.rb
+++ spec/features/date_selection_spec.rb
@@ -16,13 +16,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the last 30 days when `last 30 days` is selected' do
     to = Time.zone.today
     from = Time.zone.today - 30.days
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-30-days').click
-    click_button 'OK'
-    sleep 2
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-30-days')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -30,13 +26,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the previous month when `last month` is selected' do
     to = Time.zone.today.last_month.end_of_month
     from = Time.zone.today.last_month.beginning_of_month
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-month').click
-    click_button 'OK'
-    sleep 2
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-month')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -44,12 +36,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the last 3 months when `last 3 months` is selected' do
     to = Time.zone.today
     from = Time.zone.today - 3.months
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-3-months').click
-    click_button 'OK'
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-3-months')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -57,12 +46,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the last 6 months when `last6 months` is selected' do
     to = Time.zone.today
     from = Time.zone.today - 6.months
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-6-months').click
-    click_button 'OK'
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-6-months')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -70,13 +56,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the last year when `last year` is selected' do
     to = Time.zone.today
     from = Time.zone.today - 1.year
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-1-year').click
-    click_button 'OK'
-    sleep 2
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-1-year')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -84,12 +66,9 @@ RSpec.describe 'date selection', type: :feature do
   it 'renders data for the last 2 years when `last 2 years` is selected' do
     to = Time.zone.today
     from = Time.zone.today - 2.years
-    stub_for(from, to)
+    stub_response_for_date_range(from, to)
 
-    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
-    find('#date_range_last-2-years').click
-    click_button 'OK'
-    click_on 'Unique pageviews table'
+    visit_page_and_filter_by_date_range('last-2-years')
 
     expect_metrics_for_each_date_to_be_correct(from, to)
   end
@@ -127,11 +106,16 @@ RSpec.describe 'date selection', type: :feature do
           { "date" => (from - 1.day).to_s, "value" => 8 },
           { "date" => (from - 2.days).to_s, "value" => 8 },
           { "date" => (to + 1.day).to_s, "value" => 8 }
+        ],
+        number_of_internal_searches: [
+          { "date" => (from - 1.day).to_s, "value" => 8 },
+          { "date" => (from - 2.days).to_s, "value" => 8 },
+          { "date" => (to + 1.day).to_s, "value" => 8 }
         ]
       })
   end
 
-  def stub_for(from, to)
+  def stub_response_for_date_range(from, to)
     content_data_api_has_metric(base_path: 'base/path',
       from: from,
       to: to,
@@ -161,35 +145,43 @@ RSpec.describe 'date selection', type: :feature do
           { "date" => (from - 1.day).to_s, "value" => 10 },
           { "date" => (from - 2.days).to_s, "value" => 20 },
           { "date" => (to + 1.day).to_s, "value" => 30 }
+        ],
+        number_of_internal_searches: [
+          { "date" => (from - 1.day).to_s, "value" => 8 },
+          { "date" => (from - 2.days).to_s, "value" => 8 },
+          { "date" => (to + 1.day).to_s, "value" => 8 }
         ]
       })
   end
 
   def expect_metrics_for_each_date_to_be_correct(from, to)
-    date1 = (from - 1.day).to_s.last(5)
-    date2 = (from - 2.days).to_s.last(5)
-    date3 = (to + 1.day).to_s.last(5)
+    month_and_date_string_for_date1 = (from - 1.day).to_s.last(5)
+    month_and_date_string_for_date2 = (from - 2.days).to_s.last(5)
+    month_and_date_string_for_date3 = (to + 1.day).to_s.last(5)
 
     unique_pageviews_rows = find("#unique_pageviews_table").all('tr')
 
-    expect(unique_pageviews_rows.count).to eq 4
-    expect(unique_pageviews_rows[0].text).to eq ""
-    expect(unique_pageviews_rows[1].text).to eq "#{date1} 1"
-    expect(unique_pageviews_rows[2].text).to eq "#{date2} 2"
-    expect(unique_pageviews_rows[3].text).to eq "#{date3} 30"
+    expected_rows = ["", "#{month_and_date_string_for_date1} 1", "#{month_and_date_string_for_date2} 2", "#{month_and_date_string_for_date3} 30"]
+
+    expect(unique_pageviews_rows.map(&:text)).to match_array(expected_rows)
   end
 
   def expect_default_date_period_metrics_to_be_displayed(from, to)
-    date1 = (from - 1.day).to_s.last(5)
-    date2 = (from - 2.days).to_s.last(5)
-    date3 = (to + 1.day).to_s.last(5)
+    month_and_date_string_for_date1 = (from - 1.day).to_s.last(5)
+    month_and_date_string_for_date2 = (from - 2.days).to_s.last(5)
+    month_and_date_string_for_date3 = (to + 1.day).to_s.last(5)
 
     unique_pageviews_rows = find("#unique_pageviews_table").all('tr')
 
-    expect(unique_pageviews_rows.count).to eq 4
-    expect(unique_pageviews_rows[0].text).to eq ""
-    expect(unique_pageviews_rows[1].text).to eq "#{date1} 0"
-    expect(unique_pageviews_rows[2].text).to eq "#{date2} 9"
-    expect(unique_pageviews_rows[3].text).to eq "#{date3} 9"
+    expected_rows = ["", "#{month_and_date_string_for_date1} 0", "#{month_and_date_string_for_date2} 9", "#{month_and_date_string_for_date3} 9"]
+
+    expect(unique_pageviews_rows.map(&:text)).to match_array(expected_rows)
+  end
+
+  def visit_page_and_filter_by_date_range(date_range)
+    visit '/metrics/base/path?metrics[]=unique_pageviews&metrics[]=pageviews'
+    find("#date_range_#{date_range}").click
+    click_button 'OK'
+    click_on 'Unique pageviews table'
   end
 end
