6e6530652a0264220d1166cdc027c7bf7a814a4c
Sean Rankine <sean.rankine@me.com>
2018-10-09 12:04:39 +0100

    Add previous date range
    This adds the attribute for the previous date range.

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -12,37 +12,58 @@ class DateRange
     date_to_range[:from]
   end
 
+  def prev_from
+    date_to_range[:prev_from]
+  end
+
+  def prev_to
+    date_to_range[:prev_to]
+  end
+
+
 private
 
   def date_to_range
     case time_period
     when 'last-month'
       {
+        prev_from: (Time.zone.today - 2.months).beginning_of_month.to_s,
+        prev_to: (Time.zone.today - 2.months).end_of_month.to_s,
         from: Time.zone.today.last_month.beginning_of_month.to_s,
         to: Time.zone.today.last_month.end_of_month.to_s
       }
     when 'last-3-months'
       {
+        prev_from: (Time.zone.today - 6.months).to_s,
+        prev_to: (Time.zone.today - 3.months).to_s,
         from: (Time.zone.today - 3.months).to_s,
         to: Time.zone.today.to_s
       }
     when 'last-6-months'
       {
+        prev_from: (Time.zone.today - 12.months).to_s,
+        prev_to: (Time.zone.today - 6.months).to_s,
         from: (Time.zone.today - 6.months).to_s,
         to: Time.zone.today.to_s
       }
     when 'last-year'
       {
+        prev_from: (Time.zone.today - 2.years).to_s,
+        prev_to: (Time.zone.today - 1.year).to_s,
         from: (Time.zone.today - 1.year).to_s,
         to: Time.zone.today.to_s
       }
     when 'last-2-years'
       {
+        prev_from: (Time.zone.today - 4.years).to_s,
+        prev_to: (Time.zone.today - 2.years).to_s,
         from: (Time.zone.today - 2.years).to_s,
         to: Time.zone.today.to_s
       }
     else
       {
+        prev_from: (Time.zone.today - 60.days).to_s,
+        prev_to: (Time.zone.today - 30.days).to_s,
         from: (Time.zone.today - 30.days).to_s,
         to: Time.zone.today.to_s
       }
--- spec/models/date_range_spec.rb
+++ spec/models/date_range_spec.rb
@@ -39,4 +39,44 @@ RSpec.describe DateRange do
       expect(DateRange.new('last-month').from).to eq(Time.zone.today.last_month.beginning_of_month.to_s)
     end
   end
+  describe '#prev_to' do
+    it 'returns date of 30 days ago if selected time period is `last-30-days`' do
+      expect(DateRange.new('last-30-days').prev_to).to eq((Time.zone.today - 30.days).to_s)
+    end
+    it 'returns date of 3 months ago if selected time period is `last-3-months`' do
+      expect(DateRange.new('last-3-months').prev_to).to eq((Time.zone.today - 3.months).to_s)
+    end
+    it 'returns date of 6 months ago if selected time period is `last-6-months`' do
+      expect(DateRange.new('last-6-months').prev_to).to eq((Time.zone.today - 6.months).to_s)
+    end
+    it 'returns date of 1 year ago if selected time period is `last-year`' do
+      expect(DateRange.new('last-year').prev_to).to eq((Time.zone.today - 1.year).to_s)
+    end
+    it 'returns date of 2 years ago if selected time period is `last-2-years`' do
+      expect(DateRange.new('last-2-years').prev_to).to eq((Time.zone.today - 2.years).to_s)
+    end
+    it 'returns date of last day of 2 months ago if selected time period is `last-month`' do
+      expect(DateRange.new('last-month').prev_to).to eq((Time.zone.today - 2.months).end_of_month.to_s)
+    end
+  end
+  describe '#prev_from' do
+    it 'returns date of 60 days ago if selected time period is `last-30-days`' do
+      expect(DateRange.new('last-30-days').prev_from).to eq((Time.zone.today - 60.days).to_s)
+    end
+    it 'returns date of 6 months ago if selected time period is `last-3-months`' do
+      expect(DateRange.new('last-3-months').prev_from).to eq((Time.zone.today - 6.months).to_s)
+    end
+    it 'returns date of 12 months ago if selected time period is `last-6-months`' do
+      expect(DateRange.new('last-6-months').prev_from).to eq((Time.zone.today - 12.months).to_s)
+    end
+    it 'returns date of 2 year ago if selected time period is `last-year`' do
+      expect(DateRange.new('last-year').prev_from).to eq((Time.zone.today - 2.years).to_s)
+    end
+    it 'returns date of 5 years ago if selected time period is `last-2-years`' do
+      expect(DateRange.new('last-2-years').prev_from).to eq((Time.zone.today - 4.years).to_s)
+    end
+    it 'returns date of first day of 2 months ago if selected time period is `last-month`' do
+      expect(DateRange.new('last-month').prev_from).to eq((Time.zone.today - 2.months).beginning_of_month.to_s)
+    end
+  end
 end
